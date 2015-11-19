29a97298addb7155d3ce1374630fc52ab06651b7
JRonhovde <jronhovde90@gmail.com>
2015-11-19 22:40:32 +0000

    Add branch name autocompletion.

--- gitcomments.sh
+++ gitcomments.sh
@@ -8,4 +8,11 @@ gitcomments() {
         python ~/gitcomments/gitcomments.py $(git rev-parse --abbrev-ref HEAD) $FILE $(git config --get remote.origin.url) && vim "+copen | cfile $FILE"
     fi
 }
+_gitcomments() {
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    BRANCHES="$(git branch | tr -d '\n')" 
+    COMPREPLY=( $(compgen -W "$BRANCHES" -- $cur ) )
+    return 0
+}
+complete -F _gitcomments gitcomments
 alias vimgc='vim "+copen | cfile /usr/tmp/'$USER'-gitcomments.txt"'
